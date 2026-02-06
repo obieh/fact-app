359af26dca28228c4bd556ef16e4eea905c88db8
obieh <o_amadic@yahoo.com>
2026-02-06 12:04:50 +0100

    ....

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Script loaded successfully.");
+const btnShare = document.querySelector(".btn-share");
+const factForm = document.querySelector(".fact-form");
+
+btnShare.addEventListener("click", function () {
+  factForm.classList.toggle("hidden");
+  btnShare.textContent = factForm.classList.contains("hidden")
+    ? "Share a fact"
+    : "Close";
+});
