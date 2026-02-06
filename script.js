cf1acbcc51a597be29a3ba72a237f873751038fd
obieh <o_amadic@yahoo.com>
2026-02-06 18:45:12 +0100

    ....

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ btnShare.addEventListener("click", function () {
     ? "Share a fact"
     : "Close";
 });
+
+let votesInteresting = document.querySelector(".vote-btn--interesting");
+let votesMindblowing = document.querySelector(".vote-btn--mindblowing");
+let votesFalse = document.querySelector(".vote-btn--false");
+
+votesInteresting.addEventListener("click", function () {
+  console.log("Interesting");
+});
+
+votesMindblowing.addEventListener("click", function () {
+  console.log("Mind-blowing");
+});
+
+votesFalse.addEventListener("click", function () {
+  console.log("False");
+});
