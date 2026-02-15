a4d7f9e372e34520e60744145165a3ad346b0f9b
obieh <o_amadic@yahoo.com>
2026-02-16 00:01:32 +0100

    .....

--- script.js
+++ script.js
@@ -39,14 +39,22 @@ const factList = document.querySelector(".fact-list");
 
 //Create DOM elements: Render facts in list
 factList.innerHTML = "";
-//factList.insertAdjacentHTML("afterbegin", "<li>Here is a new fact.</li>");
+
+//Load data from supabase
+
+fetch("https://qlaezsnuyofisgboncnd.supabase.co", {
+  headers: {
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFsYWV6c251eW9maXNnYm9uY25kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3ODUzMTUsImV4cCI6MjA4NTM2MTMxNX0.jTn86m1fQb9Hy6TmAToSXyOOZSJMdTjWoW384ASS7B8",
+  },
+});
 
 const htmlArr = initialFacts.map(
   (fact) => `<li class="fact">${fact.text}</li>`,
 );
 console.log(htmlArr);
 const html = htmlArr.join("");
-factList.insertAdjacentHTML("afterbegin", html);  
+factList.insertAdjacentHTML("afterbegin", html);
 
 //Toggle form visibility
 btnShare.addEventListener("click", function () {
