d98d7b5d7344326e95ff6f471ad7c9291e717420
obieh <o_amadic@yahoo.com>
2026-02-19 02:06:05 +0100

    ....

--- script.js
+++ script.js
@@ -42,6 +42,17 @@ const initialFacts = [
   },
 ];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 //Selcting DOM elements
 const btnShare = document.querySelector(".btn-share");
 const factForm = document.querySelector(".fact-form");
@@ -143,16 +154,7 @@ const { text1, source, category } = factObj;
 console.log(text1, source);
 console.log(factObj.createSummary());
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
+
 
 const allCate = CATEGORIES.map((cat) => {
   return cat.name;
