2ac52ab2c4797674b8dd982f0eb0b1ab3124c264
obieh <o_amadic@yahoo.com>
2026-02-25 11:43:51 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import "./style.css";
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="58" width="58" alt="logo" />
-        <h1>Welcome to fact-applet.</h1>
-      </div>
+    <>
+      {/* Header */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="58" width="58" alt="logo" />
+          <h1>Welcome to fact-applet.</h1>
+        </div>
 
-      <button className="btn btn-large btn-share btn-open">Share a fact</button>
-    </header>
+        <button className="btn btn-large btn-share btn-open">
+          Share a fact
+        </button>
+      </header>
+
+      <NewFactForm />
+
+      <main class="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
