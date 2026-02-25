b3a210d9662f00816c4eb60ed0edd98d344bfe69
obieh <o_amadic@yahoo.com>
2026-02-25 12:08:29 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./style.css";
 function App() {
+  const appTitle = "Welcome to fact-applet";
+
   return (
     <>
       {/* Header */}
       <header className="header">
         <div className="logo">
           <img src="logo.png" height="58" width="58" alt="logo" />
-          <h1>Welcome to fact-applet.</h1>
+          <h1>{appTitle}</h1>
         </div>
 
         <button className="btn btn-large btn-share btn-open">
