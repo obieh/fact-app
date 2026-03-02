4ea308b9aa6b646f5126f6ac3b5cea683ba9f678
obieh <o_amadic@yahoo.com>
2026-03-02 02:23:24 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -44,17 +44,6 @@ const initialFacts = [
   },
 ];
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
-
 function App() {
   const appTitle = "Welcome to fact-applet";
 
@@ -85,6 +74,17 @@ function App() {
 function NewFactForm() {
   return <form className="fact-form">Fact form</form>;
 }
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
 
 function CategoryFilter() {
   return (
