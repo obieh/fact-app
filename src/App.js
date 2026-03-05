b3b64195c51d9568ddb1b59b8341ed8cc71d0a5f
obieh <o_amadic@yahoo.com>
2026-03-05 12:47:25 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -82,6 +82,17 @@ function Header({ showForm, setShowForm }) {
   );
 }
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function NewFactForm() {
   return (
     <form className="fact-form">
@@ -100,17 +111,6 @@ function NewFactForm() {
     </form>
   );
 }
-
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
 
 function CategoryFilter() {
   return (
