fcd5fecaf8f3db71860b2c2b2208bea8baaa237d
obieh <o_amadic@yahoo.com>
2026-03-07 01:11:53 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ function NewFactForm() {
         onChange={(e) => setSource(e.target.value)}
       />
       <select value={category} onChange={(e) => setCategory(e.target.value)}>
-        <option value="">Select Category:</option>
+        <option value="">Select Category</option>
         {CATEGORIES.map((cat) => (
           <option key={cat.name} value={cat.name}>
             {cat.name.toUpperCase()}
