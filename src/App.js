859b6f2cf18a91f2e3a0ddf64d73a48f4379eb1a
obieh <o_amadic@yahoo.com>
2026-03-21 02:33:44 +0100

    .....

--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ import supabase from "./supabase";
 
 import "./style.css";
 
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 function Counter() {
   const [count, setCount] = useState(0);
 
@@ -70,12 +104,12 @@ function Loader() {
 }
 
 function Header({ showForm, setShowForm }) {
-  const appTitle = "Welcome to factApp";
+  const appTitle = "Today I Learned";
 
   return (
     <header className="header">
       <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="factApp Logo" />
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
         <h1>{appTitle}</h1>
       </div>
 
@@ -268,7 +302,7 @@ function Fact({ fact, setFacts }) {
       <p>
         {isDisputed ? <span className="disputed">[⛔️ DISPUTED]</span> : null}
         {fact.text}
-        <a className="source" href={fact.source} target="blank">
+        <a className="source" href={fact.source} target="_blank">
           (Source)
         </a>
       </p>
